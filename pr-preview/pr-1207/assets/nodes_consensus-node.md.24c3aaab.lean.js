03361783392f7dfd915ec10aafa4b37f21038094
jcstein <jcstein@users.noreply.github.com>
2023-10-27 21:58:01 +0000

    Deploy preview for PR 1207 🛫

---
+++ pr-preview/pr-1207/assets/nodes_consensus-node.md.24c3aaab.lean.js
@@ -0,0 +1,77 @@
+import{c as o}from"./chunks/constants.e90b75b1.js";import{o as t,c as p,k as s,t as e,l,a as n,Q as a}from"./chunks/framework.f1446dd9.js";const c="/pr-preview/pr-1207/img/nodes/full-consensus-node.png",r="/pr-preview/pr-1207/img/nodes/validator.png",i=a("",17),d={class:"vp-code-group vp-adaptive-theme"},_=a("",1),h={class:"blocks"},y={class:"language-bash vp-adaptive-theme active"},u=s("button",{title:"Copy Code",class:"copy"},null,-1),E=s("span",{class:"lang"},"bash",-1),g={class:"shiki github-dark vp-code-dark"},F={class:"line"},C=a("",8),b={style:{color:"#9ECBFF"}},T={class:"shiki github-light vp-code-light"},m={class:"line"},v=a("",8),k={style:{color:"#032F62"}},A={class:"language-bash vp-adaptive-theme"},f=s("button",{title:"Copy Code",class:"copy"},null,-1),q=s("span",{class:"lang"},"bash",-1),B={class:"shiki github-dark vp-code-dark"},S={class:"line"},P=a("",8),D={style:{color:"#9ECBFF"}},I={class:"shiki github-light vp-code-light"},w={class:"line"},V=a("",8),R={style:{color:"#032F62"}},x=s("p",null,[n("Copy the "),s("code",null,"genesis.json"),n(" file:")],-1),N={class:"vp-code-group vp-adaptive-theme"},O=a("",1),$={class:"blocks"},M={class:"language-bash vp-adaptive-theme active"},L=s("button",{title:"Copy Code",class:"copy"},null,-1),H=s("span",{class:"lang"},"bash",-1),W={class:"shiki github-dark vp-code-dark"},z={class:"line"},U=s("span",{style:{color:"#B392F0"}},"cp",-1),Y=s("span",{style:{color:"#E1E4E8"}}," $HOME",-1),j={style:{color:"#9ECBFF"}},G=s("span",{style:{color:"#E1E4E8"}}," ",-1),Q=s("span",{style:{color:"#79B8FF"}},"\\",-1),J=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"    $HOME"),s("span",{style:{color:"#9ECBFF"}},"/.celestia-app/config")],-1),X={class:"shiki github-light vp-code-light"},K={class:"line"},Z=s("span",{style:{color:"#6F42C1"}},"cp",-1),ss=s("span",{style:{color:"#24292E"}}," $HOME",-1),as={style:{color:"#032F62"}},ns=s("span",{style:{color:"#24292E"}}," ",-1),os=s("span",{style:{color:"#005CC5"}},"\\",-1),es=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"    $HOME"),s("span",{style:{color:"#032F62"}},"/.celestia-app/config")],-1),ls={class:"language-bash vp-adaptive-theme"},ts=s("button",{title:"Copy Code",class:"copy"},null,-1),ps=s("span",{class:"lang"},"bash",-1),cs={class:"shiki github-dark vp-code-dark"},rs={class:"line"},is=s("span",{style:{color:"#B392F0"}},"cp",-1),ds=s("span",{style:{color:"#E1E4E8"}}," $HOME",-1),_s={style:{color:"#9ECBFF"}},hs=s("span",{style:{color:"#E1E4E8"}}," ",-1),ys=s("span",{style:{color:"#79B8FF"}},"\\",-1),us=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"    $HOME"),s("span",{style:{color:"#9ECBFF"}},"/.celestia-app/config")],-1),Es={class:"shiki github-light vp-code-light"},gs={class:"line"},Fs=s("span",{style:{color:"#6F42C1"}},"cp",-1),Cs=s("span",{style:{color:"#24292E"}}," $HOME",-1),bs={style:{color:"#032F62"}},Ts=s("span",{style:{color:"#24292E"}}," ",-1),ms=s("span",{style:{color:"#005CC5"}},"\\",-1),vs=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"    $HOME"),s("span",{style:{color:"#032F62"}},"/.celestia-app/config")],-1),ks=s("p",null,[n("Set seeds in the "),s("code",null,"$HOME/.celestia-app/config/config.toml"),n(" file:")],-1),As={class:"vp-code-group vp-adaptive-theme"},fs=a("",1),qs={class:"blocks"},Bs={class:"language-bash vp-adaptive-theme active"},Ss=s("button",{title:"Copy Code",class:"copy"},null,-1),Ps=s("span",{class:"lang"},"bash",-1),Ds={class:"shiki github-dark vp-code-dark"},Is={class:"line"},ws=a("",6),Vs={style:{color:"#9ECBFF"}},Rs=a("",12),xs=s("span",{class:"line"},[s("span",{style:{color:"#79B8FF"}},"echo"),s("span",{style:{color:"#E1E4E8"}}," $SEEDS")],-1),Ns=a("",1),Os={class:"shiki github-light vp-code-light"},$s={class:"line"},Ms=a("",6),Ls={style:{color:"#032F62"}},Hs=a("",12),Ws=s("span",{class:"line"},[s("span",{style:{color:"#005CC5"}},"echo"),s("span",{style:{color:"#24292E"}}," $SEEDS")],-1),zs=a("",1),Us=a("",1),Ys=s("p",null,[s("strong",null,"Optionally"),n(", you can set persistent peers in your "),s("code",null,"config.toml"),n(" file. You can get the persistent peers from the networks repository with the following commands:")],-1),js=s("p",null,"Setting persistent peers is advised only if you are running a sentry node.",-1),Gs={class:"vp-code-group vp-adaptive-theme"},Qs=a("",1),Js={class:"blocks"},Xs={class:"language-bash vp-adaptive-theme active"},Ks=s("button",{title:"Copy Code",class:"copy"},null,-1),Zs=s("span",{class:"lang"},"bash",-1),sa={class:"shiki github-dark vp-code-dark"},aa={class:"line"},na=a("",6),oa={style:{color:"#9ECBFF"}},ea=a("",12),la=s("span",{class:"line"},[s("span",{style:{color:"#79B8FF"}},"echo"),s("span",{style:{color:"#E1E4E8"}}," $PERSISTENT_PEERS")],-1),ta=a("",1),pa={class:"shiki github-light vp-code-light"},ca={class:"line"},ra=a("",6),ia={style:{color:"#032F62"}},da=a("",12),_a=s("span",{class:"line"},[s("span",{style:{color:"#005CC5"}},"echo"),s("span",{style:{color:"#24292E"}}," $PERSISTENT_PEERS")],-1),ha=a("",1),ya={class:"language-bash vp-adaptive-theme"},ua=s("button",{title:"Copy Code",class:"copy"},null,-1),Ea=s("span",{class:"lang"},"bash",-1),ga={class:"shiki github-dark vp-code-dark"},Fa={class:"line"},Ca=a("",6),ba={style:{color:"#9ECBFF"}},Ta=a("",12),ma=s("span",{class:"line"},[s("span",{style:{color:"#79B8FF"}},"echo"),s("span",{style:{color:"#E1E4E8"}}," $PERSISTENT_PEERS")],-1),va=a("",1),ka={class:"shiki github-light vp-code-light"},Aa={class:"line"},fa=a("",6),qa={style:{color:"#032F62"}},Ba=a("",12),Sa=s("span",{class:"line"},[s("span",{style:{color:"#005CC5"}},"echo"),s("span",{style:{color:"#24292E"}}," $PERSISTENT_PEERS")],-1),Pa=a("",1),Da=a("",58),Ia={class:"vp-code-group vp-adaptive-theme"},wa=a("",1),Va={class:"blocks"},Ra={class:"language-bash vp-adaptive-theme active"},xa=s("button",{title:"Copy Code",class:"copy"},null,-1),Na=s("span",{class:"lang"},"bash",-1),Oa={class:"shiki github-dark vp-code-dark"},$a=s("span",{class:"line"},[s("span",{style:{color:"#79B8FF"}},"cd"),s("span",{style:{color:"#E1E4E8"}}," $HOME")],-1),Ma=a("",1),La=a("",1),Ha=a("",1),Wa={class:"line"},za=s("span",{style:{color:"#9ECBFF"}},"    ",-1),Ua=s("span",{style:{color:"#B392F0"}},"egrep",-1),Ya=s("span",{style:{color:"#9ECBFF"}}," ",-1),ja=s("span",{style:{color:"#79B8FF"}},"-o",-1),Ga={style:{color:"#9ECBFF"}},Qa=a("",6),Ja=a("",1),Xa=a("",1),Ka={class:"shiki github-light vp-code-light"},Za=s("span",{class:"line"},[s("span",{style:{color:"#005CC5"}},"cd"),s("span",{style:{color:"#24292E"}}," $HOME")],-1),sn=a("",1),an=a("",1),nn=a("",1),on={class:"line"},en=s("span",{style:{color:"#032F62"}},"    ",-1),ln=s("span",{style:{color:"#6F42C1"}},"egrep",-1),tn=s("span",{style:{color:"#032F62"}}," ",-1),pn=s("span",{style:{color:"#005CC5"}},"-o",-1),cn={style:{color:"#032F62"}},rn=a("",6),dn=a("",1),_n=a("",1),hn={class:"language-bash vp-adaptive-theme"},yn=s("button",{title:"Copy Code",class:"copy"},null,-1),un=s("span",{class:"lang"},"bash",-1),En={class:"shiki github-dark vp-code-dark"},gn=s("span",{class:"line"},[s("span",{style:{color:"#79B8FF"}},"cd"),s("span",{style:{color:"#E1E4E8"}}," $HOME")],-1),Fn=a("",1),Cn=a("",1),bn=a("",1),Tn={class:"line"},mn=s("span",{style:{color:"#9ECBFF"}},"    ",-1),vn=s("span",{style:{color:"#B392F0"}},"egrep",-1),kn=s("span",{style:{color:"#9ECBFF"}}," ",-1),An=s("span",{style:{color:"#79B8FF"}},"-o",-1),fn={style:{color:"#9ECBFF"}},qn=a("",6),Bn=a("",1),Sn=a("",1),Pn={class:"shiki github-light vp-code-light"},Dn=s("span",{class:"line"},[s("span",{style:{color:"#005CC5"}},"cd"),s("span",{style:{color:"#24292E"}}," $HOME")],-1),In=a("",1),wn=a("",1),Vn=a("",1),Rn={class:"line"},xn=s("span",{style:{color:"#032F62"}},"    ",-1),Nn=s("span",{style:{color:"#6F42C1"}},"egrep",-1),On=s("span",{style:{color:"#032F62"}}," ",-1),$n=s("span",{style:{color:"#005CC5"}},"-o",-1),Mn={style:{color:"#032F62"}},Ln=a("",6),Hn=a("",1),Wn=a("",1),zn=a("",24),Un={class:"language-bash vp-adaptive-theme"},Yn=s("button",{title:"Copy Code",class:"copy"},null,-1),jn=s("span",{class:"lang"},"bash",-1),Gn={class:"shiki github-dark vp-code-dark"},Qn=a("",1),Jn=a("",1),Xn={class:"line"},Kn=s("span",{style:{color:"#E1E4E8"}},"--from=$VALIDATOR_WALLET ",-1),Zn={style:{color:"#79B8FF"}},so=s("span",{style:{color:"#E1E4E8"}}," ",-1),ao=s("span",{style:{color:"#79B8FF"}},"\\",-1),no=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"--fees=21000utia")],-1),oo={class:"shiki github-light vp-code-light"},eo=a("",1),lo=a("",1),to={class:"line"},po=s("span",{style:{color:"#24292E"}},"--from=$VALIDATOR_WALLET ",-1),co={style:{color:"#005CC5"}},ro=s("span",{style:{color:"#24292E"}}," ",-1),io=s("span",{style:{color:"#005CC5"}},"\\",-1),_o=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"--fees=21000utia")],-1),ho=a("",30),yo={class:"language-bash vp-adaptive-theme"},uo=s("button",{title:"Copy Code",class:"copy"},null,-1),Eo=s("span",{class:"lang"},"bash",-1),go={class:"shiki github-dark vp-code-dark"},Fo=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"MONIKER"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"your_moniker"')],-1),Co=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"validator"')],-1),bo=s("span",{class:"line"},null,-1),To=a("",1),mo=a("",1),vo=a("",1),ko=a("",1),Ao={class:"line"},fo=s("span",{style:{color:"#E1E4E8"}},"    ",-1),qo={style:{color:"#79B8FF"}},Bo=s("span",{style:{color:"#E1E4E8"}}," ",-1),So=s("span",{style:{color:"#79B8FF"}},"\\",-1),Po=a("",1),Do=a("",1),Io=a("",1),wo=a("",1),Vo=a("",1),Ro=a("",1),xo=a("",1),No=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"    "),s("span",{style:{color:"#79B8FF"}},"--gas=220000")],-1),Oo={class:"shiki github-light vp-code-light"},$o=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"MONIKER"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"your_moniker"')],-1),Mo=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"validator"')],-1),Lo=s("span",{class:"line"},null,-1),Ho=a("",1),Wo=a("",1),zo=a("",1),Uo=a("",1),Yo={class:"line"},jo=s("span",{style:{color:"#24292E"}},"    ",-1),Go={style:{color:"#005CC5"}},Qo=s("span",{style:{color:"#24292E"}}," ",-1),Jo=s("span",{style:{color:"#005CC5"}},"\\",-1),Xo=a("",1),Ko=a("",1),Zo=a("",1),se=a("",1),ae=a("",1),ne=a("",1),oe=a("",1),ee=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"    "),s("span",{style:{color:"#005CC5"}},"--gas=220000")],-1),le=a("",43),_e=JSON.parse('{"title":"Setting up a Celestia full consensus node","description":"Learn how to set up a Celestia consensus node.","frontmatter":{"description":"Learn how to set up a Celestia consensus node.","head":[["meta",{"name":"og:title","content":"Setting up a Celestia full consensus node | Celestia Docs"},{"name":"og:description","content":false}]]},"headers":[],"relativePath":"nodes/consensus-node.md","filePath":"nodes/consensus-node.md","lastUpdated":1698443824000}'),te={name:"nodes/consensus-node.md"},he=Object.assign(te,{setup(pe){return(ce,re)=>(t(),p("div",null,[i,s("div",d,[_,s("div",h,[s("div",y,[u,E,s("pre",g,[s("code",null,[s("span",F,[C,s("span",b,e(l(o).mochaChainId),1)])])]),s("pre",T,[s("code",null,[s("span",m,[v,s("span",k,e(l(o).mochaChainId),1)])])])]),s("div",A,[f,q,s("pre",B,[s("code",null,[s("span",S,[P,s("span",D,e(l(o).arabicaChainId),1)])])]),s("pre",I,[s("code",null,[s("span",w,[V,s("span",R,e(l(o).arabicaChainId),1)])])])])])]),x,s("div",N,[O,s("div",$,[s("div",M,[L,H,s("pre",W,[s("code",null,[s("span",z,[U,Y,s("span",j,"/networks/"+e(l(o).mochaChainId)+"/genesis.json",1),G,Q]),n(`
+`),J])]),s("pre",X,[s("code",null,[s("span",K,[Z,ss,s("span",as,"/networks/"+e(l(o).mochaChainId)+"/genesis.json",1),ns,os]),n(`
+`),es])])]),s("div",ls,[ts,ps,s("pre",cs,[s("code",null,[s("span",rs,[is,ds,s("span",_s,"/networks/"+e(l(o).arabicaChainId)+"/genesis.json",1),hs,ys]),n(`
+`),us])]),s("pre",Es,[s("code",null,[s("span",gs,[Fs,Cs,s("span",bs,"/networks/"+e(l(o).arabicaChainId)+"/genesis.json",1),Ts,ms]),n(`
+`),vs])])])])]),ks,s("div",As,[fs,s("div",qs,[s("div",Bs,[Ss,Ps,s("pre",Ds,[s("code",null,[s("span",Is,[ws,s("span",Vs," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).mochaChainId)+"/seeds.txt ",1),Rs]),n(`
+`),xs,n(`
+`),Ns])]),s("pre",Os,[s("code",null,[s("span",$s,[Ms,s("span",Ls," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).mochaChainId)+"/seeds.txt ",1),Hs]),n(`
+`),Ws,n(`
+`),zs])])]),Us])]),Ys,js,s("div",Gs,[Qs,s("div",Js,[s("div",Xs,[Ks,Zs,s("pre",sa,[s("code",null,[s("span",aa,[na,s("span",oa," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).mochaChainId)+"/peers.txt ",1),ea]),n(`
+`),la,n(`
+`),ta])]),s("pre",pa,[s("code",null,[s("span",ca,[ra,s("span",ia," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).mochaChainId)+"/peers.txt ",1),da]),n(`
+`),_a,n(`
+`),ha])])]),s("div",ya,[ua,Ea,s("pre",ga,[s("code",null,[s("span",Fa,[Ca,s("span",ba," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).arabicaChainId)+"/peers.txt ",1),Ta]),n(`
+`),ma,n(`
+`),va])]),s("pre",ka,[s("code",null,[s("span",Aa,[fa,s("span",qa," https://raw.githubusercontent.com/celestiaorg/networks/master/"+e(l(o).arabicaChainId)+"/peers.txt ",1),Ba]),n(`
+`),Sa,n(`
+`),Pa])])])])]),Da,s("div",Ia,[wa,s("div",Va,[s("div",Ra,[xa,Na,s("pre",Oa,[s("code",null,[$a,n(`
+`),Ma,n(`
+`),La,n(`
+`),Ha,n(`
+`),s("span",Wa,[za,Ua,Ya,ja,s("span",Ga,' ">'+e(l(o).mochaChainId)+'.*tar" ',1),Qa]),n(`
+`),Ja,n(`
+`),Xa])]),s("pre",Ka,[s("code",null,[Za,n(`
+`),sn,n(`
+`),an,n(`
+`),nn,n(`
+`),s("span",on,[en,ln,tn,pn,s("span",cn,' ">'+e(l(o).mochaChainId)+'.*tar" ',1),rn]),n(`
+`),dn,n(`
+`),_n])])]),s("div",hn,[yn,un,s("pre",En,[s("code",null,[gn,n(`
+`),Fn,n(`
+`),Cn,n(`
+`),bn,n(`
+`),s("span",Tn,[mn,vn,kn,An,s("span",fn,' ">'+e(l(o).arabicaChainId)+'.*tar" ',1),qn]),n(`
+`),Bn,n(`
+`),Sn])]),s("pre",Pn,[s("code",null,[Dn,n(`
+`),In,n(`
+`),wn,n(`
+`),Vn,n(`
+`),s("span",Rn,[xn,Nn,On,$n,s("span",Mn,' ">'+e(l(o).arabicaChainId)+'.*tar" ',1),Ln]),n(`
+`),Hn,n(`
+`),Wn])])])])]),zn,s("div",Un,[Yn,jn,s("pre",Gn,[s("code",null,[Qn,n(`
+`),Jn,n(`
+`),s("span",Xn,[Kn,s("span",Zn,"--chain-id="+e(l(o).mochaChainId),1),so,ao]),n(`
+`),no])]),s("pre",oo,[s("code",null,[eo,n(`
+`),lo,n(`
+`),s("span",to,[po,s("span",co,"--chain-id="+e(l(o).mochaChainId),1),ro,io]),n(`
+`),_o])])]),ho,s("div",yo,[uo,Eo,s("pre",go,[s("code",null,[Fo,n(`
+`),Co,n(`
+`),bo,n(`
+`),To,n(`
+`),mo,n(`
+`),vo,n(`
+`),ko,n(`
+`),s("span",Ao,[fo,s("span",qo,"--chain-id="+e(l(o).mochaChainId),1),Bo,So]),n(`
+`),Po,n(`
+`),Do,n(`
+`),Io,n(`
+`),wo,n(`
+`),Vo,n(`
+`),Ro,n(`
+`),xo,n(`
+`),No])]),s("pre",Oo,[s("code",null,[$o,n(`
+`),Mo,n(`
+`),Lo,n(`
+`),Ho,n(`
+`),Wo,n(`
+`),zo,n(`
+`),Uo,n(`
+`),s("span",Yo,[jo,s("span",Go,"--chain-id="+e(l(o).mochaChainId),1),Qo,Jo]),n(`
+`),Xo,n(`
+`),Ko,n(`
+`),Zo,n(`
+`),se,n(`
+`),ae,n(`
+`),ne,n(`
+`),oe,n(`
+`),ee])])]),le]))}});export{_e as __pageData,he as default};
